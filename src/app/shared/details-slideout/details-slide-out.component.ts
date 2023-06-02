296b9c9de3ff635c42101104bd0200d5563d8165
ankita <asood@octolabs.io>
2023-06-02 12:47:23 -0500

    fix: fix details slide-out data error

--- src/app/shared/details-slideout/details-slide-out.component.ts
+++ src/app/shared/details-slideout/details-slide-out.component.ts
@@ -1,7 +1,6 @@
 import {ChangeDetectionStrategy, Component, HostBinding, inject, Input, OnInit, Signal, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MarvelService} from "../../store/marvel.service";
-import {toSignal} from "@angular/core/rxjs-interop";
 import {Character} from "../../characters/characters.model";
 import {Comic} from "../../comics/comics.model";
 import {Creator} from "../../creators/creators.model";
@@ -14,67 +13,53 @@ import {ShareLinkComponent} from "../share-link/share-link.component";
     standalone: true,
     imports: [CommonModule, RouterLink, ResponseHeadingPipe, ShareLinkComponent],
     template: `
-        <ng-container *ngIf="(response$ | async)[0]  as response">
-            <header class="slide-out--header">
-                <h1><a [href]="response.urls[0].url" target="_blank">{{response | responseHeading }}</a></h1>
-                <div class="slide-out--actions">
-                    <app-share-link></app-share-link>
-                    <button class="button button-icon" [routerLink]="['/', { outlets: { details : null } }]"
-                            (click)="closeSlideOutPanel()">
-                        <i class="material-icons">close</i>
-                    </button>
-                </div>
+        <ng-container *ngIf="(response$|async) as response">
+            <ng-container *ngIf="response[0] as data">
+                <header class="slide-out&#45;&#45;header">
+                    <h1><a [href]="data.urls[0].url" target="_blank">{{data | responseHeading }}</a></h1>
+                    <div class="slide-out&#45;&#45;actions">
+                        <app-share-link></app-share-link>
+                        <button class="button button-icon" [routerLink]="['/', { outlets: { details : null } }]"
+                                (click)="closeSlideOutPanel()">
+                            <i class="material-icons">close</i>
+                        </button>
+                    </div>
 
-            </header>
-            <section class="slide-out--content">
-                <article class="slide-out--info">
-                    <p>{{response.description}}</p>
-                    <p *ngIf="response.comics?.available">Comics Available: {{response.comics.available}}</p>
-                    <p *ngIf="response.series?.available">Series Available: {{response.series.available}}</p>
-                    <p *ngIf="response.stories?.available">Stories Available: {{response.series.available}}</p>
-                </article>
-                <article class="slide-out--image" *ngIf="response.thumbnail">
-                    <img [src]="response.thumbnail.path + '.' + response.thumbnail.extension" alt="thumbnail">
-                </article>
-            </section>
+                </header>
+                <section class="slide-out&#45;&#45;content">
+                    <article class="slide-out&#45;&#45;info">
+                        <p>{{data.description}}</p>
+                        <p *ngIf="data.comics?.available">Comics Available: {{data.comics.available}}</p>
+                        <p *ngIf="data.series?.available">Series Available: {{data.series.available}}</p>
+                        <p *ngIf="data.stories?.available">Stories Available: {{data.series.available}}</p>
+                    </article>
+                    <article class="slide-out&#45;&#45;image" *ngIf="data.thumbnail">
+                        <img [src]="data.thumbnail.path + '.' + data.thumbnail.extension" alt="thumbnail">
+                    </article>
+                </section>
+            </ng-container>
         </ng-container>
+
     `,
     styleUrls: ['./details-slide-out.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DetailsSlideOutComponent implements OnInit{
-    constructor(private route: ActivatedRoute) {
-    }
+export class DetailsSlideOutComponent {
     private marvelService = inject(MarvelService);
-   /* idSig = signal('');
-    typeSig = signal('');*/
-    /*@Input({required: true}) set id(id: string) {
-        this.idSig.set(id);
-    };
-    @Input({required: true}) set type(type: string) {
-        this.typeSig.set(type);
-    };*/
 
     id$ = this.route.params.pipe(map((params) => params['id']));
     type$ = this.route.params.pipe(map((params) => params['type']));
 
-    response$ = combineLatest([
-        this.type$.pipe(filter(t => t !== null)),
-    this.id$.pipe(filter(i => i !== null))]).pipe(
-        switchMap(([type,id]) => this.marvelService.getDetails(type, id))
-    )
-
-    @HostBinding('class.slide-in') slideIn = true;
-
-    // response$!: Observable<Character | Comic | Creator | undefined>;
-
-
-    ngOnInit(): void {
-        // this.response$ = this.marvelService.getDetails(this.typeSig(), this.idSig());
-        // this.response$ = this.marvelService.getDetails(this.typeSig(), this.idSig());
+    constructor(private route: ActivatedRoute) {
     }
 
+    response$ = combineLatest([
+        this.type$.pipe(filter(t => t !== null)),
+        this.id$.pipe(filter(i => i !== null))]).pipe(
+        switchMap(([type, id]) => this.marvelService.getDetails(type, id))
+    ).pipe(filter(response => !!response))
 
+    @HostBinding('class.slide-in') slideIn = true;
     closeSlideOutPanel() {
         this.slideIn = false;
     }
